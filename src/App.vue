906a8ac7794c891b09c93e55dcdf3849bfc77fe0
Manuel Guariglia <manuelguariglia@gmail.com>
2021-12-01 11:38:01 +0100

    fix header

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,22 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HEADER />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
+import HEADER from './components/HEADER.vue';
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HEADER
   }
 }
 </script>
 
 <style lang="scss">
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  @import url('./assets/style/generals.scss');
 }
 </style>
---
+++ src/components/HEADER.vue
@@ -0,0 +1,38 @@
+<template>
+  <header>
+    <img src="../assets/img/logo.png" alt="">
+    <div class="serach-bar">
+      <input type="text" placeholder="Cerca un titolo...">
+    </div>
+  </header>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss" scoped>
+@import url('../assets/style/generals.scss');
+header{
+  width: 100%;
+  height: 90px;
+  padding: 25px;
+  background-color: black;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  img{
+    width: 200px;
+  }
+  input[type=text] {
+  border: 3px solid red;
+  padding: 5px;
+  height: 50px;
+  width: 350px;
+  border-radius: 4px;
+  font-size: 25px;
+  }
+}
+</style>
