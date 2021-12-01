cc626f059c553b4d9cb0427cf7ed5f8a5e42984c
Manuel Guariglia <manuelguariglia@gmail.com>
2021-12-01 16:45:01 +0100

    fix axios

--- src/App.vue
+++ src/App.vue
@@ -2,11 +2,12 @@
   <div id="app">
     <HEADER />
     <MAIN />
+    <div>{{cards.length}}</div>
   </div>
 </template>
 
 <script>
-
+import axios from 'axios';
 import HEADER from './components/HEADER.vue';
 import MAIN from './components/MAIN.vue';
 export default {
@@ -14,7 +15,30 @@ export default {
   components: {
     HEADER,
     MAIN
-  }
+  },
+  data(){
+    return{
+      cards: [],
+      apiUrl: 'https://api.themoviedb.org/3/search/movie?api_key=cf3a8ad796de1dd7af76b38dc8ed5676&language=it-IT&query=scarface',
+      loaded: false,
+
+    }
+  },
+  methods:{
+    getApi(){
+      axios.get(this.apiUrl)
+        .then( r => {
+          this.cards = r.data.results;
+          this.loaded = true;
+        })
+        .catch( e => {
+          console.log(e);
+        })
+    },
+  },
+  mounted(){
+    this.getApi();
+  },
 }
 </script>
 
